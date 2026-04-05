6c14499d821a9646be21d67faeef1262d25292c9
Manumarichandana <msaichandana213@gmail.com>
2026-04-04 22:14:56 -0400

    Update output.tf

--- s3/output.tf
+++ s3/output.tf
@@ -1,4 +1,8 @@
 output "bucket_name" {
     description = "display the name of the bucket"
-    value       =  aws_s3_bucket.my-bucket.id
+    value       =  aws_s3_bucket.this.bucket
+}
+output "bucket_id" {
+  description = "ID of the S3 bucket"
+  value       = aws_s3_bucket.this.id
 }
